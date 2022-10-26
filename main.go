726d77fa5c097a390cb07b096cf2c88919551aa0
qianyu <qianyu@xiaoduotech.com>
2022-10-26 15:50:09 +0800

    init

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+const BaseUploadPath = "/var/file"
+
+func main() {
+	http.HandleFunc("/upload", handleUpload)
+	http.HandleFunc("/download", handleDownload)
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal("Server run fail")
+	}
+}
+
+func handleUpload(w http.ResponseWriter, request *http.Request) {
+	//文件上传只允许POST方法
+	if request.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, _ = w.Write([]byte("Method not allowed"))
+		return
+	}
+
+	//从表单中读取文件
+	file, fileHeader, err := request.FormFile("file")
+	if err != nil {
+		_, _ = io.WriteString(w, "Read file error")
+		return
+	}
+	//defer 结束时关闭文件
+	defer file.Close()
+	log.Println("filename: " + fileHeader.Filename)
+
+	//创建文件
+	newFile, err := os.Create(BaseUploadPath + "/" + fileHeader.Filename)
+	if err != nil {
+		_, _ = io.WriteString(w, "Create file error")
+		return
+	}
+	//defer 结束时关闭文件
+	defer newFile.Close()
+
+	//将文件写到本地
+	_, err = io.Copy(newFile, file)
+	if err != nil {
+		_, _ = io.WriteString(w, "Write file error")
+		return
+	}
+	_, _ = io.WriteString(w, "Upload success")
+}
+
+func handleDownload(w http.ResponseWriter, request *http.Request) {
+	//文件上传只允许GET方法
+	if request.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, _ = w.Write([]byte("Method not allowed"))
+		return
+	}
+	//文件名
+	filename := request.FormValue("filename")
+	if filename == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = io.WriteString(w, "Bad request")
+		return
+	}
+	log.Println("filename: " + filename)
+	//打开文件
+	file, err := os.Open(BaseUploadPath + "/" + filename)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = io.WriteString(w, "Bad request")
+		return
+	}
+	//结束后关闭文件
+	defer file.Close()
+
+	//设置响应的header头
+	w.Header().Add("Content-type", "application/octet-stream")
+	w.Header().Add("content-disposition", "attachment; filename=\""+filename+"\"")
+	//将文件写至responseBody
+	_, err = io.Copy(w, file)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = io.WriteString(w, "Bad request")
+		return
+	}
+}
